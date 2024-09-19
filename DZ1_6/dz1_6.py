1850cc76b0b45dd4ba90237e98ff29513f50bb95
st4gg3r <n.kobitskoy@gmail.com>
2024-09-19 14:41:01 +0300

    поправил

--- DZ1_6/dz1_6.py
+++ DZ1_6/dz1_6.py
@@ -1,20 +1,25 @@
-# sport = ['Бокс', 'Теннис']
-# sport.append(input('Введите свой любимый вид спорта: ').capitalize())
-# print(sorted(sport))
-from pprint import pprint
+def sports():
+    sport.append(input('Введите свой любимый вид спорта: ').capitalize())
+    print(sorted(sport))
 
-school_learning = ['Математика','Физика','Химия','Информатика','Философия','История']
 def print_learning():
     for i in sorted(school_learning):
         print(i, end=' ')
     print()
 
 def remove_learning():
+    """
+    т.к. в задании сказано вывести список)))) а не list вывожу список человеческий)), просто люблю фишки  множеств
+    """
+
     remove = input('Введите через зяпатую предметы которые хотите удалить: ').title()
     remove = remove.replace(' ','')
     return set(remove.split(','))
 
 if __name__ == '__main__':
+    sport = ['Бокс', 'Теннис']
+    school_learning = ['Математика', 'Физика', 'Химия', 'Информатика', 'Философия', 'История']
+    sports()
     print_learning()
     school_learning = set(school_learning) - remove_learning()
     print_learning()
